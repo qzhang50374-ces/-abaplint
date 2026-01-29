947f3adfc8afaf1c019271af63f4cd696f4c88b2
qiwei <qiwei.zhang@jast.co.jp>
2026-01-29 12:16:42 +0900

    fix: correct publicPath in webpack config for GitHub Pages subpath deployment

--- webpack.config.js
+++ webpack.config.js
@@ -4,69 +4,74 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = ({mode} = {mode: "development"}) => ({
-  entry: {
-    "app": "./src/index.ts",
-    "editor.worker": "monaco-editor/esm/vs/editor/editor.worker.js",
-    "json.worker": "monaco-editor/esm/vs/language/json/json.worker.js",
-  },
-  mode,
-  output: {
-    path: path.join(__dirname, "docs"),
-    filename: "[name].bundle.js",
-    publicPath: mode === "production" ? "/-abaplint/" : "/",
-    globalObject: "self",
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
+module.exports = (env, argv) => {
+  const mode = argv.mode || "development";
+  const isProduction = mode === "production";
+  
+  return {
+    entry: {
+      "app": "./src/index.ts",
+      "editor.worker": "monaco-editor/esm/vs/editor/editor.worker.js",
+      "json.worker": "monaco-editor/esm/vs/language/json/json.worker.js",
     },
-    open: true,
-    hot: true,
-  },
-  resolve: {
-    fallback: {
-      "buffer": require.resolve("buffer/"),
-      "path": require.resolve("path-browserify"),
-      "stream": require.resolve("stream-browserify"),
-      "crypto": false,
+    mode,
+    output: {
+      path: path.join(__dirname, "docs"),
+      filename: "[name].bundle.js",
+      publicPath: isProduction ? "/-abaplint/" : "/",
+      globalObject: "self",
     },
-    extensions: [".js", ".ts", ".tsx"],
-  },
-  module: {
-    rules: [
-      {test: /\.css$/, use: ["style-loader", "css-loader"]},
-      {test: /\.less$/, use: ["style-loader", "css-loader", "less-loader"]},
-      {
-        test: /\.png$/,
-        include: /favicon/,
-        use: "file-loader?name=[name].[ext]",
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "public"),
       },
-      {
-        test: /\.png$|\.svg$/,
-        exclude: /favicon/,
-        use: "url-loader?limit=1024",
+      open: true,
+      hot: true,
+    },
+    resolve: {
+      fallback: {
+        "buffer": require.resolve("buffer/"),
+        "path": require.resolve("path-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "crypto": false,
       },
-      {
-        test: /\.ttf$/,
-        type: "asset/resource",
-        generator: {
-          filename: "[name][ext]",
+      extensions: [".js", ".ts", ".tsx"],
+    },
+    module: {
+      rules: [
+        {test: /\.css$/, use: ["style-loader", "css-loader"]},
+        {test: /\.less$/, use: ["style-loader", "css-loader", "less-loader"]},
+        {
+          test: /\.png$/,
+          include: /favicon/,
+          use: "file-loader?name=[name].[ext]",
         },
-      },
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-        exclude: /node_modules/,
-      },
+        {
+          test: /\.png$|\.svg$/,
+          exclude: /favicon/,
+          use: "url-loader?limit=1024",
+        },
+        {
+          test: /\.ttf$/,
+          type: "asset/resource",
+          generator: {
+            filename: "[name][ext]",
+          },
+        },
+        {
+          test: /\.tsx?$/,
+          loader: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "public/index.html",
+      }),
+      new webpack.ProvidePlugin({
+        Buffer: ["buffer", "Buffer"],
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "public/index.html",
-    }),
-    new webpack.ProvidePlugin({
-      Buffer: ["buffer", "Buffer"],
-    }),
-  ],
-});
+  };
+};
